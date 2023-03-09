227e91d7551fb9eab3f01343e2641f0a102bc99b
judekabeya <judek9392@gmail.com>
2023-03-09 18:59:06 +0100

    update

--- src/routes/users.js
+++ src/routes/users.js
@@ -2,13 +2,13 @@ const express = require ("express");
 
 const router = express.Router();
 const userControllers = require("../controllers/users.js");
-const {createUser, getAllUser, getUserWatchlist, updateUserInfo } = require('../controllers/users')
+const {createUser, getAllUser, getUserWatchlist, updateUserInfo, addFavoris } = require('../controllers/users')
 
 router.route('/users/create').post(createUser) ;
 router.route('/users/all').get(getAllUser);
 router.route('/users/getUserWatchlist/:owner').get(getUserWatchlist);
 router.route('/users/update/:id').post(updateUserInfo);
-router.route('/users/:userId/favorites/:watchlistId');
+router.route('/users/:userId/favorites/:watchlistId').post(addFavoris);
 
 
 
